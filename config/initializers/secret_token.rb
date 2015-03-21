09f2e7dabf55a8def55cd41e618ad0375cf9f3e1
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-20 19:41:07 -0500

    initial commit...let the games begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskmaster::Application.config.secret_token = 'f3a7b3d9a7e7213333d2e8d0ba02fbfcc2254fed642061b7961ff30081b2d8e49c249d24f20658bf369068c94359e387b49ac2ba854ef6bbc7512736a38dc079'
