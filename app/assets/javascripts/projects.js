8e21ee07c68c94c131f4e1e4baf123f8d02009c0
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-23 11:58:51 -0500

    basic tabs working for open/closed tasks

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,44 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var navTabs = [];
+var tabs = [];
+var new_task_button,
+veil;
+window.onload = function(){
+  var links = document.getElementsByClassName("tab_head");
+  for (var i = 0; i < links.length; i ++ ) {
+    var id = getHash( links[i].getAttribute ("href") );
+    navTabs[id] = links[i];
+    tabs[id] = document.getElementById(id);
+  }
+  var i = 0;
+  for ( id in navTabs ){
+    navTabs[id].addEventListener("click", swapTab);
+    i ++;
+  }
+  veil = document.getElementById("veil")
+  new_task_button = document.getElementById("new_task_button");
+  new_task_button.addEventListener("click", showNewForm);
+}
+
+
+var swapTab = function(e){
+  e.preventDefault()
+  var selectedId = getHash( this.getAttribute('href') );
+  
+  for (var id in tabs){
+    if ( id == selectedId ) {
+      navTabs[id].className = 'tab_head active';
+      tabs[id].className = 'tab_content';
+    } else {
+      navTabs[id].className = 'tab_head';
+      tabs[id].className = 'tab_content hidden';
+    }
+  }
+  return false
+}
+
+function getHash( url ) {
+  var hashPos = url.lastIndexOf ( '#' );
+  return url.substring( hashPos + 1 );
+}
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,11 +2,6 @@
 // All this logic will automatically be available in application.js.
 var new_task_button,
 veil;
-window.onload = function(){
-  veil = document.getElementById("veil")
-  new_task_button = document.getElementById("new_task_button");
-  new_task_button.addEventListener("click", showNewForm);
-}
 
 var showNewForm = function(e){
   veil.classList.toggle("hidden");
